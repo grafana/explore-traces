da4886e576d314812a4248e2177748284259dffd
André Pereira <adrapereira@gmail.com>
2024-02-14 13:13:43 +0000

    cleanup

--- src/components/testIds.ts
+++ src/components/testIds.ts
@@ -5,18 +5,4 @@ export const testIds = {
     apiUrl: 'data-testid ac-api-url',
     submit: 'data-testid ac-submit-form',
   },
-  pageOne: {
-    container: 'data-testid pg-one-container',
-    navigateToFour: 'data-testid navigate-to-four',
-  },
-  pageTwo: {
-    container: 'data-testid pg-two-container',
-  },
-  pageThree: {
-    container: 'data-testid pg-three-container',
-  },
-  pageFour: {
-    container: 'data-testid pg-four-container',
-    navigateBack: 'data-testid navigate-back',
-  },
 };
