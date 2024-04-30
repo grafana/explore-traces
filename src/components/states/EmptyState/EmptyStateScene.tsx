596c1520b39fba94665fa66c8e4dc3fa3d82a679
Joey Tawadrous <joey.tawadrous@grafana.com>
2024-04-30 09:54:23 +0100

    Update empty state

--- src/components/states/EmptyState/EmptyStateScene.tsx
+++ src/components/states/EmptyState/EmptyStateScene.tsx
@@ -4,11 +4,12 @@ import { EmptyState } from './EmptyState';
 
 interface EmptyStateSceneState extends SceneObjectState {
   message: string;
+  imgWidth?: number;
 }
 
 export class EmptyStateScene extends SceneObjectBase<EmptyStateSceneState> {
   public static Component = ({ model }: SceneComponentProps<EmptyStateScene>) => {
-    const { message } = model.useState();
-    return <EmptyState message={message} />;
+    const { message, imgWidth } = model.useState();
+    return <EmptyState message={message} imgWidth={imgWidth} />;
   };
 }
