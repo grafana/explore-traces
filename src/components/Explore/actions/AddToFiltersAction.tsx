66ff221fe76458fe4b4d348993bfba51b1f0ac9d
Joey Tawadrous <joey.tawadrous@grafana.com>
2024-09-05 09:54:30 +0100

    Fix primary signal being removed with filter

--- src/components/Explore/actions/AddToFiltersAction.tsx
+++ src/components/Explore/actions/AddToFiltersAction.tsx
@@ -46,7 +46,8 @@ export class AddToFiltersAction extends SceneObjectBase<AddToFiltersActionState>
 export const addToFilters = (variable: AdHocFiltersVariable, label: string, value: string) => {
   // ensure we set the new filter with latest value
   // and remove any existing filter for the same key
-  const filtersWithoutNew = variable.state.filters.filter((f) => f.key !== label);
+  // and also keep span.db.name as it is a primary filter
+  const filtersWithoutNew = variable.state.filters.filter((f) => f.key === 'span.db.name' || f.key !== label);
 
   variable.setState({
     filters: [
