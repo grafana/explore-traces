d9cbadf7eae3cdc4c7cfa0c048ac8c03bfa12df2
Joey Tawadrous <joey.tawadrous@grafana.com>
2024-09-26 12:51:08 +0100

    Fix exception with structure tab

--- src/utils/trace-merge/merge.ts
+++ src/utils/trace-merge/merge.ts
@@ -13,48 +13,50 @@ export function mergeTraces(traces: TraceSearchMetadata[]): TreeNode {
     traceID: '',
   });
 
-  for (const trace of traces) {
-    if (trace.spanSets?.length !== 1) {
-      throw new Error('there should be only 1 spanset!');
-    }
+  if (traces && traces.length > 0) {
+    for (const trace of traces) {
+      if (trace.spanSets?.length !== 1) {
+        throw new Error('there should be only 1 spanset!');
+      }
 
-    const traceStartTime = parseInt(trace.startTimeUnixNano || '0', 10);
+      const traceStartTime = parseInt(trace.startTimeUnixNano || '0', 10);
 
-    const ss = trace.spanSets[0];
-    // sort by nestedSetLeft
-    ss.spans.sort((s1, s2) => nestedSetLeft(s1) - nestedSetLeft(s2));
+      const ss = trace.spanSets[0];
+      // sort by nestedSetLeft
+      ss.spans.sort((s1, s2) => nestedSetLeft(s1) - nestedSetLeft(s2));
 
-    // reset curNode to root each loop to re-overlay the next trace
-    let curNode: TreeNode = tree;
-    // left/right is only valid w/i a trace, so reset it each loop
-    resetLeftRight(tree);
-    for (const span of ss.spans) {
-      // force traceID to be the same for all spans in a trace
-      span.traceId = trace.traceID;
-      span.startTimeUnixNano = `${parseInt(span.startTimeUnixNano, 10) - traceStartTime}`;
+      // reset curNode to root each loop to re-overlay the next trace
+      let curNode: TreeNode = tree;
+      // left/right is only valid w/i a trace, so reset it each loop
+      resetLeftRight(tree);
+      for (const span of ss.spans) {
+        // force traceID to be the same for all spans in a trace
+        span.traceId = trace.traceID;
+        span.startTimeUnixNano = `${parseInt(span.startTimeUnixNano, 10) - traceStartTime}`;
 
-      // walk up the tree until we find a node that is a parent of this span
-      while (curNode.parent !== null) {
-        if (curNode.isChild(span)) {
-          break;
+        // walk up the tree until we find a node that is a parent of this span
+        while (curNode.parent !== null) {
+          if (curNode.isChild(span)) {
+            break;
+          }
+          curNode = curNode.parent;
         }
-        curNode = curNode.parent;
-      }
 
-      // is there an already existing child that matches the span?
-      const child = curNode.findMatchingChild(span);
-      if (child) {
-        child.addSpan(span);
-        // to the next span!
-        curNode = child;
-        continue;
-      }
+        // is there an already existing child that matches the span?
+        const child = curNode.findMatchingChild(span);
+        if (child) {
+          child.addSpan(span);
+          // to the next span!
+          curNode = child;
+          continue;
+        }
 
-      // if not, create a new child node and make it the cur node
-      const newNode = createNode(span);
-      newNode.traceID = trace.traceID;
-      curNode.addChild(newNode);
-      curNode = newNode;
+        // if not, create a new child node and make it the cur node
+        const newNode = createNode(span);
+        newNode.traceID = trace.traceID;
+        curNode.addChild(newNode);
+        curNode = newNode;
+      }
     }
   }
 
