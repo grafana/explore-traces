11228dd47ee016e3e830880518d0063bc8144747
Andrej Ocenas <mr.ocenas@gmail.com>
2024-11-11 16:12:54 +0100

    Update router stuff

--- src/components/Routes/Routes.tsx
+++ src/components/Routes/Routes.tsx
@@ -1,19 +1,13 @@
 import React from 'react';
-import { Redirect, Switch } from 'react-router-dom';
-import { CompatRoute } from 'react-router-dom-v5-compat';
+import { Route, Routes as ReactRoutes, Navigate } from 'react-router-dom-v5-compat';
 import { TraceExplorationPage } from '../../pages/Explore';
-import { PLUGIN_BASE_URL, ROUTES } from 'utils/shared';
+import { ROUTES } from 'utils/shared';
 
 export const Routes = () => {
   return (
-    <Switch>
-      <CompatRoute path={prefixRoute(`${ROUTES.Explore}`)} component={TraceExplorationPage} />
-      <Redirect to={prefixRoute(ROUTES.Explore)} />
-    </Switch>
+    <ReactRoutes>
+      <Route path={ROUTES.Explore} element={<TraceExplorationPage />} />
+      <Route path={'/'} element={<Navigate replace to={ROUTES.Explore}/> } />
+    </ReactRoutes>
   );
 };
-
-// Prefixes the route with the base URL of the plugin
-function prefixRoute(route: string): string {
-  return `${PLUGIN_BASE_URL}/${route}`;
-}
