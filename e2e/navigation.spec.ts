72ddbe421245abaccf53efa8abbda4f8fde55688
Joey Tawadrous <joey.tawadrous@grafana.com>
2024-11-13 15:29:58 +0000

    Remove flakey test

--- e2e/navigation.spec.ts
+++ e2e/navigation.spec.ts
@@ -18,10 +18,4 @@ test.describe('navigating app', () => {
     await expect(page.getByText('Data source')).toBeVisible();
     await explorePage.assertMissingData();
   });
-
-  test('menu click should visit default explore traces url', async ({ page }) => {
-    await explorePage.click(page.getByTestId('data-testid Toggle menu'));
-    await explorePage.click(page.getByTestId('data-testid navigation mega-menu').getByRole('link', { name: 'Traces' }));
-    await expect(page).toHaveURL("http://localhost:3001/a/grafana-exploretraces-app/explore?primarySignal=full_traces&from=now-15m&to=now&var-ds=tempo&var-filters=nestedSetParent%7C%3C%7C0&var-metric=rate&var-groupBy=resource.service.name&var-latencyThreshold=&var-partialLatencyThreshold=&refresh=&actionView=breakdown");
-  });
 });
