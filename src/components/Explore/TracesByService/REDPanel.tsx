5dbf765a2831e976338970bbe80afc0fa96534b2
André Pereira <adrapereira@gmail.com>
2025-02-03 10:29:33 +0000

    Fix RED Panel crash

--- src/components/Explore/TracesByService/REDPanel.tsx
+++ src/components/Explore/TracesByService/REDPanel.tsx
@@ -151,13 +151,15 @@ export class REDPanel extends SceneObjectBase<RateMetricsPanelState> {
         parent.subscribeToState((newState, prevState) => {
           if (data.state.data?.state === LoadingState.Done) {
             if (!isEqual(newState.selection, prevState.selection) || newState.actionView !== prevState.actionView) {
-              const annotations = this.buildSelectionAnnotation(newState);
-              data.setState({
-                data: {
-                  ...data.state.data!,
-                  annotations: annotations,
-                },
-              });
+              if (this.isDuration()) {
+                const annotations = this.buildSelectionAnnotation(newState);
+                data.setState({
+                  data: {
+                    ...data.state.data!,
+                    annotations: annotations,
+                  },
+                });
+              }
             }
           }
         })
@@ -299,7 +301,7 @@ export class REDPanel extends SceneObjectBase<RateMetricsPanelState> {
 
 export const getYBuckets = (series: DataFrame[]) => {
   return series.map((s) => parseFloat(s.fields[1].name)).sort((a, b) => a - b);
-}
+};
 
 export const getMinimumsForDuration = (yBuckets: number[]) => {
   const slowestBuckets = Math.floor(yBuckets.length / 4);
@@ -308,11 +310,11 @@ export const getMinimumsForDuration = (yBuckets: number[]) => {
     minBucket = 0;
   }
 
-  return { 
-    minDuration: yBucketToDuration(minBucket - 1, yBuckets), 
-    minBucket
+  return {
+    minDuration: yBucketToDuration(minBucket - 1, yBuckets),
+    minBucket,
   };
-}
+};
 
 function getStyles(theme: GrafanaTheme2) {
   return {
