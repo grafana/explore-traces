cb6eb171c904d69dde544ec7f83bf425fa5f98f3
André Pereira <adrapereira@gmail.com>
2025-02-06 13:04:12 +0000

    Add title to drawer again

--- src/pages/Explore/TraceExploration.tsx
+++ src/pages/Explore/TraceExploration.tsx
@@ -250,7 +250,7 @@ export class TraceExplorationScene extends SceneObjectBase {
           <div className={styles.body}>{topScene && <topScene.Component model={topScene} />}</div>
         </div>
         {drawerScene && traceId && (
-          <Drawer size={'lg'} onClose={() => traceExploration.closeDrawer()}>
+          <Drawer title={`View trace ${traceId}`} size={'lg'} onClose={() => traceExploration.closeDrawer()}>
             <drawerScene.Component model={drawerScene} />
           </Drawer>
         )}
